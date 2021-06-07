fad6dda115779d3451c03b733023cdcb9bfd6439
Abbos95 <abbos.abdullaev.w96@mail.ru>
2021-06-07 17:17:49 +0900

    two commit

---
+++ script/script.js
@@ -0,0 +1,105 @@
+'use strict';
+
+
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const mar = +prompt('18ga kirganmisiz?', '');
+
+// console.log(mar + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Ismingiz nima?', '');
+// answers[1] = prompt('Familyangiz nima?', '');
+// answers[2] = prompt('Nechi yoshdasiz?', '');
+
+// document.write(answers);
+
+
+// const answer = +prompt('18 yoshdan otganmisiz?', '');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Ismingiz nima?', '');
+// answers[1] = prompt('Familyangiz nima?', '');
+// answers[2] = prompt('Qayerda turasiz?', '');
+// answers[3] = prompt('Yoshingiz nechida?', '');
+
+// document.write(answers);
+
+// const cotegory = 'toys';
+
+// console.log('https//someurl.com/' + cotegory + '/' + '4');
+
+// console.log(`https://someurl.com/${cotegory}/5`);
+
+// const user = prompt('Ismingiz nima?', '');
+
+// alert(`Salom, ${user}`);
+
+// const user = prompt('Ismingiz nima?', '');
+
+// alert(`Salom ${user}`);
+
+// const abbos = [];
+
+// abbos[0] = prompt('Ismingiz?', '');
+// abbos[1] = prompt('Familyangiz?', '');
+// abbos[2] = prompt('Yashash joyingiz?', '');
+
+// const abbos2 = prompt(`${abbos} ma'lumotlar to'g'riligiga aminmisiz?`, '');
+
+// document.write(abbos2);
+
+// console.log('arr' + '-object');
+
+// console.log(4 + + "5");
+
+
+// console.log('arr' + "- object");
+// console.log(4 + +"5");
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2*4 === 8);
+
+const isChecked = confirm('18ga kirganmisiz?'),
+      isClose = confirm('Turmushga chiqganmisiz?'),
+      isOpen = confirm('Hudoga ishonasizmi?');
+
+      console.log( !isClose);
+      document.write(isClose && isChecked && isOpen);
+
+      console.log(2+2*2 != 8);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
